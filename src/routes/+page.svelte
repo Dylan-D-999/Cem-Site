776e7d00ff8fce6befd25731aafeb036e7ab9621
Dylan Dennis <30069665@students.southwales.ac.uk>
2025-11-18 12:35:04 +0000

    Testing functionality from new machine

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<h1>Onegai</h1>
+<h1>Testing Header</h1>
 <p>
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
   documentation
+</p>
+<p>
+  Test Page start with npn run dev
 </p>
