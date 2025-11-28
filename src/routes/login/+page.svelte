ed77f3322b51cfdfdd6c30c3af53f2b6cd8b001a
Dylan D <30069665@students.southwales.ac.uk>
2025-11-28 15:02:06 +0000

    Added Sign in with google function to login page and hooked into firebase auth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,29 @@
-<script>
+<script lang="ts">
   import SignupForm from "$lib/components/signup-form.svelte";
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { auth } from "$lib/firebase";
+
+  async function loginWithGoogle() {
+    //Google Login Logic
+    console.log("Logging in with Google...");
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      console.log("User logged in:", result.user);
+    } catch (e) {
+      console.error("Login error:", e);
+    }
+  }
+  async function loginWithMeta() {
+    //Probably wont add Meta just as a test for func calls
+    console.log("Logging in with Meta...");
+  }
 </script>
 
 <div
   class="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
 >
   <div class="w-full max-w-sm md:max-w-4xl">
-    <SignupForm />
+    <SignupForm onGoogleLogin={loginWithGoogle} onMetaLogin={loginWithMeta} />
   </div>
 </div>
