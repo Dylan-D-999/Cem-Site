57b852a5bafdc3b957a3641e1d51fc56db9b3b44
AliyaAburas <aliyaaburas77@hotmail.com>
2026-01-13 16:27:47 +0000

    Front Page Information
    Edited the front page to include information at
    the bottom in a more visually appealing way.
    Used columns for this with the help of my
    colleague Kerry.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
   import * as ButtonGroup from "$lib/components/ui/button-group/index.js";
   import { Button } from "$lib/components/ui/button/index.js";
   import { goto } from "$app/navigation";
-    import SignupForm from "$lib/components/signup-form.svelte";
 
   //Function to go to sign in page defined in /src/routes/login
   function goToSignInPage() {
@@ -20,14 +19,42 @@
 
 <style>
 
-h1{
-  font-size: 35px;
-  text-align: center;
-}
-h2 {
-  font-size: 20px;
-  text-align: center;
-}
+  h1{
+    font-size: 35px;
+    text-align: center;
+  }
+  h2 {
+    font-size: 20px;
+    text-align: center;
+  }
+  .info-section {
+    margin: 40px 0;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
+
+  .column {
+    padding: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+  }
+
+  .page-container { 
+    min-height: 100vh; 
+    display: flex; 
+    flex-direction: column;
+    justify-content: flex-end; 
+    padding: 2rem; 
+  }
+  .info-row { 
+    display: flex; 
+    gap: 2rem; 
+    padding: 40px 0;
+    justify-content: center;
+  }
+  .info-section { 
+    text-align: center; 
+  }
 
 </style>
 
@@ -43,9 +70,34 @@ h2 {
   <Button href="/login?mode=signup">Sign up</Button>
 </ButtonGroup.Root>
 
+<div class="page-container">
+<div class="info-row">
+  <div class="column">
+    <section class="info-section">
+      <h2> Purpose Built </h2>
+      <p> A competent and modern <br>
+        project management solution <br>
+        built for teams of all sized </p>
+    </section>
+  </div>
+  <div class="column">
+    <section class="info-section">
+      <h2> Minimalistic Design </h2>
+      <p> Designed in a minimalist style <br>
+        with user experience in mind <br>
+        for maximum productivity </p>
+    </section>
+  </div>
+  <div class="column">
+    <section class="info-section">
+      <h2> Simple Onboarding </h2>
+      <p> Simple design and a streamlined <br>
+        feature set that allows fast onboarding <br>
+        and utilisation in teams </p>
+    </section>
+  </div>
+</div>
+</div>
 
-<Row>
-  <Col>
- 
-  </Col>
-</Row>
+
+  
